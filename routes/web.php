70f97d00b60d8533056561a824892ec31ed2140a
Ozols <its27eo1@odin.local>
2018-06-13 14:19:08 +0300

    onetomany Crud completed

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\User;
+use App\Post;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,4 +16,56 @@
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/create', function(){
+    $user = User::findOrFail(1);
+    $post = new Post;
+    $post->title = "newwww";
+    $post->body = "neeeeeeewwww";
+    $user->post()->save($post);
+});
+
+Route::get('/read', function()
+{
+    $user = User::findOrFail(1);
+    $posts = Post::whereUserId(1)->get();
+    //dd($posts);
+    // return $user->posts;
+
+    foreach($user->posts as $post)
+    {
+        echo "Title: " . $post->title . "<br>";
+        echo "Body: " . $post->body . "<br>";
+    }
+});
+
+Route::get('/update', function()
+{
+    $userId = 1;
+    $user = User::findOrFail($userId);
+    $post = $userId ? Post::findOrFail(2) : new Post;
+    $post->title = "Edited post";
+    $post->body = "Updated body";
+
+    $user->post()->save($post);
+
+    return "Success";
+
+    // $user = User::find(1);
+    // $user->post()->whereId(1)->update(['title'=>'asdasdsa','body'=>'cxvxcvcxvx']);
+
+});
+
+Route::get('/delete', function()
+{
+    $userId = 1;
+    $user = User::findOrFail($userId);
+    $post = Post::whereUserId($userId)
+        ->first()
+        ->delete();
+
+    // $user->post()->delete();
+
+    return "Deleted";
 });
